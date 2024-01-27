47e26a54ebb96bf1ffb62d871327e35913abdf8d
Angelica <Angelica137@users.noreply.github.com>
2024-01-27 20:55:11 +0000

    print correct squence, green

--- squares.py
+++ squares.py
@@ -2,9 +2,9 @@ def squares_to_odd(a, b):
     total = (a*a) - (b*b)
     x = 2*b + 1
     y = 2*a - 1
-    seq = []
+    seq = ""
     while x < y:
-        seq.append(x)
+        seq += str(x) + " + "
         x += 2
-    seq.append(y)
-    return f'{total}, {seq}'
+    seq += str(y)
+    return f'{a}^2 - {b}^2 = {seq} = {total}'

49e0299727436440052522fbb8c4d0cb2e1a8168
Angelica <Angelica137@users.noreply.github.com>
2024-01-27 20:22:32 +0000

    complete test suite, green

--- test_squares.py
+++ test_squares.py
@@ -1,5 +1,13 @@
 from squares import squares_to_odd
 
 
-def test_squares_to_odd():
+def test_squares_to_odd_56():
     assert squares_to_odd(9, 5) == 56
+
+
+def test_squares_to_odd_56():
+    assert squares_to_odd(10, 7) == 51
+
+
+def test_squares_to_odd_56():
+    assert squares_to_odd(100, 98) == 396

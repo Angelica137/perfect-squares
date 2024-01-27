834983741e3e5d8c18fa4e1dc101be38a1dd1b0d
Angelica <Angelica137@users.noreply.github.com>
2024-01-27 20:44:56 +0000

    return the correct string, red

--- test_squares.py
+++ test_squares.py
@@ -2,16 +2,16 @@ from squares import squares_to_odd
 
 
 def test_squares_to_odd_56():
-    assert squares_to_odd(9, 5) == "56, [11, 13, 15, 17]"
+    assert squares_to_odd(9, 5) == "9^2 - 5^2 = 11 + 13 + 15 + 17 = 56"
 
 
 def test_squares_to_odd_51():
-    assert squares_to_odd(10, 7) == "51, [15, 17, 19]"
+    assert squares_to_odd(10, 7) == "10^2 - 7^2 = 15 + 17 + 19 = 51"
 
 
 def test_squares_to_odd_396():
-    assert squares_to_odd(100, 98) == "396, [197, 199]"
+    assert squares_to_odd(100, 98) == "100^2 - 98^2 = 197 + 199 = 396"
 
 
 def test_squares_to_odd_199():
-    assert squares_to_odd(100, 99) == "199, [199]"
+    assert squares_to_odd(100, 99) == "100^2 - 99^2 = 199 = 199"

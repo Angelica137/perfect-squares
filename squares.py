223520c1a4bff3a869afff5292d3ec720de8750f
Angelica <Angelica137@users.noreply.github.com>
2024-01-27 20:40:39 +0000

    return the sequence

--- squares.py
+++ squares.py
@@ -2,4 +2,9 @@ def squares_to_odd(a, b):
     total = (a*a) - (b*b)
     x = 2*b + 1
     y = 2*a - 1
-    return f'{total}, {x}, {y}'
+    seq = []
+    while x < y:
+        seq.append(x)
+        x += 2
+    seq.append(y)
+    return f'{total}, {seq}'
